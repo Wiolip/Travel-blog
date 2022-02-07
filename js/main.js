90e7ea274b14781521feb7f10d5c2787194def6c
Wioleta Lipinska <hello@wiolipcreates.pl>
2022-02-07 16:20:13 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+
+    // sticky navigation
+
+    window.addEventListener("scroll", function () {
+      var nav = document.querySelector("nav");
+      nav.classList.toggle("sticky", window.scrollY > 0)
+    });
+
+
+
+
